a1f3bf4adb74269bea34917e31b6bc96ab4f5cff
ahmetyusufyabaneri <ahmetyusuf1yabaneri@gmail.com>
2025-03-06 23:49:41 +0300

    updated

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -5,17 +5,23 @@ const SideBar = () => {
   return (
     <aside className="h-screen flex flex-col justify-between items-center p-3 max-md:p-2 max-md:gap-20 max-md:justify-normal">
       <img src="/logo.jpg" className="max-w-20 md:max-w-40" />
-      <nav>
+      <nav className="flex flex-col gap-20">
         {links.map((link) => (
           <NavLink
             to={link.path}
-            className="flex items-center text-xl text-gray-400"
+            className="flex items-center gap-4 text-xl text-gray-400"
           >
-            <span>{link.icon}</span>
-            <span>{link.title}</span>
+            <span className="max-md:text-2xl">{link.icon}</span>
+            <span className="max-md:hidden">{link.title}</span>
           </NavLink>
         ))}
       </nav>
+      <div className="flex flex-col gap-4 max-md:hidden">
+        <p className="font-semibold">Daily News</p>
+        <button className="bg-red-400 text-white p-2 rounded-lg cursor-pointer hover:bg-red-500">
+          Subscribe
+        </button>
+      </div>
     </aside>
   );
 };
